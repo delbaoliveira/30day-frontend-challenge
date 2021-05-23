0fc8358a7f1559203461c01dfdd3a3c284d85813
Delba de Oliveira <delba.brown@gmail.com>
2021-05-23 19:49:20 +0100

    Update site title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>30 Day Front-End Challenge</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
