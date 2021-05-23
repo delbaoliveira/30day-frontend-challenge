880c550798d3662b9552594241a93b92eb83e275
Delba de Oliveira <delba.brown@gmail.com>
2021-05-23 19:57:07 +0100

    Create Button component

---
+++ components/Button.js
@@ -0,0 +1,9 @@
+const Button = ({ children }) => {
+  return (
+    <button className="md:w-48 w-11/12  bg-gray-900 hover:bg-white hover:text-gray-800 border border-transparent hover:border-gray-800 text-white py-2.5 px-2 rounded m-2 transition duration-200 focus:outline-none">
+      {children}
+    </button>
+  )
+}
+
+export default Button
