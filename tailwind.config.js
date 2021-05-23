351d100f9077ac3ed0c7ca295f29f42d57955ca1
Delba de Oliveira <delba.brown@gmail.com>
2021-05-23 19:49:09 +0100

    Add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["Roboto Mono", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
