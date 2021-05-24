34c6e3791d6a30a875ee894ee09e51135c5c8402
Delba de Oliveira <delba.brown@gmail.com>
2021-05-24 10:34:52 +0100

    Fix timer on safari

--- components/Timer.js
+++ components/Timer.js
@@ -2,18 +2,15 @@ import React from "react"
 import { useTimer } from "react-timer-hook"
 
 const Timer = () => {
-  const expiryTimestamp = new Date("May 24, 2021 24:00:00")
-  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds())
-
   const { seconds, minutes, hours, days } = useTimer({
-    expiryTimestamp,
+    expiryTimestamp: 1621983600000,
     onExpire: () => console.warn("onExpire called"),
     autoStart: true,
   })
 
   return (
-    <div className="polka-bg bg-black font-mono text-center h-full rounded-b-lg flex flex-col justify-center ">
-      <div className="md:text-7xl text-4xl text-white font-medium">
+    <div className="polka-bg bg-black font-mono text-center h-full rounded-b-lg flex flex-col justify-center py-10">
+      <div className="md:text-5xl text-4xl text-white font-medium">
         <span>
           {days < 10 ? `0` : ""}
           {days}
